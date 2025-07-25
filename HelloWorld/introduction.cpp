4692e0c22041edc5c504a929c3add8ea68f57e66
alvarolopez6 <alvarolopezagullo@gmail.com>
2025-07-25 20:57:08 +0200

    Maths expressions and outputs

---
+++ HelloWorld/introduction.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+/* DECLARACION DE VARIABLES
+int main() {
+    int file_size = 100;
+    int counter = 0;
+    double sales = 9.99;
+    std::cout << file_size;
+    return 0;
+}
+*/
+
+/* CONSTANTES
+int main() {
+    const double pi = 3.141592653589793;
+    //pi = 0; ERROR, INASIGNABLE NUEVO VALOR A CONSTANTE
+    return 0;
+}
+*/
+
+/* OPERADORES MATEMATICOS
+int main() {
+    int x = 10;
+    int y = 3;
+    int z = x + y;
+    /*
+    int z = x - y;
+    int z = x / y; UNA DE LAS VARIABLES DEBE SER TIPO "DOUBLE"
+    int z = x % y;
+    int z =
+    //
+    std::cout << z;
+    return 0;
+}
+*/
+
+/*
+int main() {
+    int x = 10;
+    int y = x++; // x = 11, y = 10
+    int z = ++x; // x = 11, z = 11
+    return 0
+}
+*/
+
+/*
+using namespace std;
+int main() {
+    int x = 10;
+    int y = 20;
+    cout << "x = " << x << endl
+              << "y = " << y; // endl --> END-LINE
+    return 0;
+}
+*/
+
+
+int exercise() {
+    int total_sales = 95000;
+    double state_tax = .04 * total_sales;
+    double county_tax = .02 * total_sales;
+
+    cout << "Total sales: " << total_sales << endl;
+    cout << "State tax: " << state_tax << endl;
+    cout << "County tax: " << county_tax;
+
+    return 0;
+}
+
+// NEXT: READING FROM THE CONSOLE
+
+
+
+int main() {
+
+    return 0;
+}
