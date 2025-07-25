316176b5b7315c1a6275bb5602adefe92ed2038e
alvarolopez6 <alvarolopezagullo@gmail.com>
2025-07-25 19:46:59 +0200

    Introducción a variables

---
+++ HelloWorld/variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+/* DECLARACION DE VARIABLES
+int main() {
+    int file_size = 100;
+    int counter = 0;
+    double sales = 9.99;
+    std::cout << file_size;
+    return 0;
+}
+*/
+
+/* CONSTANTES
+int main() {
+    const double pi = 3.141592653589793;
+    //pi = 0; ERROR, INASIGNABLE NUEVO VALOR A CONSTANTE
+    return 0;
+}
+*/
+
+int main() {
+
+}
